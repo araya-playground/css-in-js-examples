6128f6b21c335a4add601a8106203fe7beabb9c1
arayaryoma <r1o.ryoma.abe@gmail.com>
2020-04-10 18:36:59 +0900

    Add styled example

--- preact/src/astroturf/card.tsx
+++ preact/src/astroturf/card.tsx
@@ -1,25 +1,26 @@
 import {FunctionComponent, h} from "preact";
-import {css} from 'astroturf';
+import styled , {css} from 'astroturf';
 
 const styles = css`
 .card {
-background-color: #ffffff;
+  background-color: #ffffff;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
   height: 400px;
   width: 300px;
   padding: 20px;
 }
-  .cardTitle{
+`
+
+const CardTitle = styled('span')`
   color: #6450a1;
   font-size: 2rem;
   display: inline-block;
   margin: 1em;
-  }
-`
+`;
 export const CardWithAstroturf: FunctionComponent = () => {
     return (
         <div className={styles.cardTitle}>
-            <span className={styles.card}>Card Example</span>
+            <CardTitle>Card Example</CardTitle>
         </div>
     );
 };
