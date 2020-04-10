8f0c4d3d343e9b3c2c055c02521080de61559754
arayaryoma <r1o.ryoma.abe@gmail.com>
2020-04-10 11:55:31 +0900

    Change the directory structure

---
+++ preact/emotion/src/index.tsx
@@ -0,0 +1,37 @@
+import fastify from "fastify";
+import render from "preact-render-to-string";
+import { Card } from "./card";
+import { h } from "preact";
+import { renderStylesToString } from "emotion-server";
+
+const server = fastify({});
+
+const App: preact.FunctionComponent = () => {
+  return (
+    <html lang="k">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Document</title>
+      </head>
+      <body>
+        <Card />
+      </body>
+    </html>
+  );
+};
+
+server.get("/", (request, reply) => {
+  reply.type("text/html");
+  const html = renderStylesToString(render(<App />));
+  reply.send(html);
+});
+
+server.listen(3000, (err, address) => {
+  if (err) throw err;
+  console.log(`server is listening on ${address}`);
+});
