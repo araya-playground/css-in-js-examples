9b31714be5823eb73ea289073892a67359a8d0e3
arayaryoma <r1o.ryoma.abe@gmail.com>
2020-04-13 16:28:47 +0900

    Use build-in http module

--- preact/jsxstyle/src/index.tsx
+++ preact/jsxstyle/src/index.tsx
@@ -1,33 +1,35 @@
-import {FunctionComponent, h} from "preact";
-import fastify from "fastify";
-import render from 'preact-render-to-string';
-import {CardWithJsxstyle} from "./card";
+import { FunctionComponent, h } from "preact";
+import render from "preact-render-to-string";
+import { CardWithJsxstyle } from "./card";
+import http, { RequestListener } from "http";
 
-const server = fastify({});
-const App: FunctionComponent = props => {
-    return (
-        <html lang="en">
-        <head>
-            <meta charSet="UTF-8"/>
-            <meta
-                name="viewport"
-                content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-            />
-            <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
-            <title>Document</title>
-        </head>
-        <body>
-        {props.children}
-        </body>
-        </html>
-    );
+const App: FunctionComponent = (props) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <title>Document</title>
+      </head>
+      <body>{props.children}</body>
+    </html>
+  );
+};
+const requestListener: RequestListener = (req, res) => {
+  res.setHeader("status", 200);
+  res.setHeader("Content-Type", "text/html");
+  res.end(
+    render(
+      <App>
+        <CardWithJsxstyle />
+      </App>
+    )
+  );
 };
 
-server.get("/", (request, reply) => {
-    reply.type("text/html");
-    reply.send(render(<App><CardWithJsxstyle/></App>))
-});
-server.listen(3000, (err, address) => {
-    if (err) throw err;
-    console.log(`server is listening on ${address}`);
-});
+const server = http.createServer(requestListener);
+server.listen(3000);
